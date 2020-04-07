6b44aa15d679b8daef1609e96e4ab679b4aee515
khDev01 <kasimh2001@outlook.com>
2020-04-07 15:53:03 +0100

    fix nav

--- nav-bar.php
+++ nav-bar.php
@@ -3,12 +3,21 @@
     // Define each name associated with an URL
     $urls = array(
       'home' => '/',
-      'html' => '/html',
-    // …
+      'html' => '/html.php',
+      'css' => '/css.php',
+      'JavaScript' => '/js.php',
+      'PHP' => '/php.php',
+      'mySQL' => '/mysql.php',
+      'python' => '/python.php',
+      'home' => '/',
     );
 
     foreach ($urls as $name => $url) {
-      print '<a '.(($currentPage === $name) ? ' class="active" ': '').' href="'.$url.'">'.$name.'</a></li>';
+      print '<a '.(($currentPage === $name) ? ' class="active" ': '').' href="'.$url.'">'.$name.'</a>';
     }
+
+    // if ($currentPage === css) {
+    //   // code...
+    // }
   ?>
 </nav>
