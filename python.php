d7d6e95e8e9890245662ec655874b598ccc3c945
khDev01 <kasimh2001@outlook.com>
2020-04-07 15:26:35 +0100

    create page layout

---
+++ python.php
@@ -0,0 +1,10 @@
+<?php $title = 'Home'; ?>
+<?php $metaTags = 'tag1 tag2'; ?>
+<?php $currentPage = 'index'; ?>
+<?php include('head.php'); ?>
+<body>
+<?php include('nav-bar.php'); ?>
+
+
+
+<?php include('footer.php'); ?>
